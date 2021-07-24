a5bc39344ad1fa56a5a9fa8fb96f6a8ffba5ba88
DESKTOP-V5GUBPT\Ray <sui16783@gmail.com>
2021-07-24 16:14:34 +0800

    no message

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //顯示所有資料
+    public function index() {
+        $data = array();
+        $data['name'] = "Ray";
+
+        return view('home', [
+            'data' => $data
+        ]);
+    }
+    //新增一筆資料
+    public function create() {
+        //
+    }
+
+    //儲存資料
+    public function store(Request $request) {
+        //
+    }
+
+    //顯示一筆資料
+    public function show($id) {
+        //
+    }
+
+    //編輯一筆資料
+    public function edit($id) {
+        //
+    }
+
+    //更新一筆資料
+    public function update(Request $request, $id) {
+        //
+    }
+
+    //刪除一筆資料
+    public function destroy($id) {
+        //
+    }
+}
